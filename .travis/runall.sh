e150327970bb09ab420a005b8f150e289ca144af
O.Z <oleg.v.zhukov@gmail.com>
2019-03-11 13:03:30 +0200

    issue #4 - setup g++-7 as a compiler for linux platform

--- .travis/runall.sh
+++ .travis/runall.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 set -ev
+
+if [ "$PLATFORM" == "linux" ]; then
+    export CXX=$COMPILER
+    ${CXX} --version
+fi
+
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
     ./mk $PLATFORM --develop
 else
